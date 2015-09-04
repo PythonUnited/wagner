108679e2b93f44e8358d88ebc22c43bc03bd3a33
Wietze Helmantel <helmantel@wyldebeast-wunderliebe.com>
2015-09-04 17:28:21 +0200

    Egg preparation in progress

--- fablib.py
+++ fablib.py
@@ -1,3 +1,4 @@
+import re
 import os
 import tempfile
 from fabric.api import sudo, settings, put, run, lcd, local, prefix, cd
@@ -8,27 +9,22 @@ from git import Repo
 
 
 def success(text, bold=True):
-
     print green(text, bold=bold)
 
 
 def info(text, bold=True):
-
     print blue(text, bold=bold)
 
 
 def error(text, bold=True):
-
     print red(text, bold=bold)
 
 
 def warning(text, bold=True):
-
     print yellow(text, bold=bold)
 
 
 def install_system_requirements(requirements, system_services=[], os="debian"):
-
     """ Install required packages system wide. """
 
     info("Installing system wide dependencies")
@@ -55,7 +51,6 @@ def install_system_requirements(requirements, system_services=[], os="debian"):
 
 
 def _system_services_action(services, action, system="upstart"):
-
     with settings(warn_only=True):
 
         for service in services:
@@ -67,7 +62,6 @@ def _system_services_action(services, action, system="upstart"):
 
 
 def start_system_services(services, system="upstart"):
-
     info("Starting services")
 
     _system_services_action(services, "start")
@@ -76,7 +70,6 @@ def start_system_services(services, system="upstart"):
 
 
 def stop_system_services(services, system="upstart"):
-
     info("Stopping services")
 
     _system_services_action(services, "stop")
@@ -85,7 +78,6 @@ def stop_system_services(services, system="upstart"):
 
 
 def restart_system_services(services, system="upstart"):
-
     info("Restarting services")
 
     _system_services_action(services, "restart")
@@ -94,7 +86,6 @@ def restart_system_services(services, system="upstart"):
 
 
 def check_sanity(env, tag=None):
-
     """ Check local GIT for uncommited stuff """
 
     info("Checking sanity...")
@@ -110,7 +101,6 @@ def check_sanity(env, tag=None):
     if tag:
 
         if tag not in [t.name for t in repo.tags]:
-
             error("No such tag exist on repository!")
             return False
 
@@ -142,7 +132,6 @@ def check_sanity(env, tag=None):
 
 
 def get_version(tag=None):
-
     """ Either use tag from command line, or use short version
     of latest commit. """
 
@@ -159,13 +148,10 @@ def get_version(tag=None):
 
 
 def generate_egg_info(eggs):
-
     info = {}
 
     for egg in eggs:
-
-        with lcd(egg):
-
+        with lcd("/tmp/%s" % egg):
             version = local('python setup.py --version', capture=True)
             name = local('python setup.py --name', capture=True)
 
@@ -175,7 +161,6 @@ def generate_egg_info(eggs):
 
 
 def upload_buildout_cfg(env, env_user, env_home, eggs, dist):
-
     TPL = """[buildout]
 extends = buildout-%s.cfg
 
@@ -187,7 +172,6 @@ extends = buildout-%s.cfg
     info = generate_egg_info(eggs)
 
     for egg in info.keys():
-
         versions.append("%s = %s" % (egg, info[egg]))
 
     cfg = TPL % (env, "\n".join(versions))
@@ -208,7 +192,6 @@ extends = buildout-%s.cfg
 
 
 def create_buildout_dist(buildout_path=".", tag=None):
-
     repo = Repo(buildout_path)
 
     dist_dir = tempfile.mkdtemp()
@@ -221,7 +204,6 @@ def create_buildout_dist(buildout_path=".", tag=None):
 
 
 def upload_buildout(dist, dest, env_user):
-
     """ upload the buildout git archive """
 
     info("Uploading buildout %s" % dist)
@@ -237,16 +219,46 @@ def upload_buildout(dist, dest, env_user):
     success('uploaded buildout')
 
 
-def upload_eggs(eggs, dest, py_version, env_user):
-
+def prepare_eggs(eggs, env):
+    """
+    Locally prepare the eggs using the branches found in autocheckout buildout
+    files like autocheckout-tst.cfg
+    :param eggs: eggs to prepare
+    :param env: either tst, acc or prd etc.
+    :return:
+    """
     for egg in eggs:
+        regex = re.compile("%s\s=\s(\S*)\s(\S*)\srev=(.*)$" % egg)
 
-        name, version = generate_egg_info([egg]).items()[0]
+        with open("autocheckout-%s.cfg" % env) as f:
+            for line in f:
+                result = regex.search(line)
+                if result:
+                    break
+
+        repo_url = result.group(2)
+        branch = result.group(3)
+        repo = None
+
+        if os.path.isdir("/tmp/%s" % egg):
+            repo = Repo("/tmp/%s" % egg)
+        if not repo:
+            info("Cloning %s, branch=%s" % (egg, branch))
+            repo = Repo().clone_from(repo_url, "/tmp/%s" % egg, branch=branch)
+
+        gitcmd = repo.git
+        gitcmd.checkout(result.group(3))
+
+        success("Prepared %s, branch=%s" % (egg, branch))
+
+
+def upload_eggs(eggs, dest, py_version, env_user):
+    for egg in eggs:
+        name, version = generate_egg_info(["/tmp/%s" % egg]).items()[0]
 
         name = name.replace("-", "_")
 
-        with lcd(egg):
-
+        with lcd("/tmp/%s" % egg):
             local("python setup.py bdist_egg")
 
             egg_name = "%s-%s-py%s.egg" % (name, version, py_version)
@@ -259,7 +271,6 @@ def upload_eggs(eggs, dest, py_version, env_user):
 
 
 def summarize(version, eggs):
-
     lines = ["***********************************"]
     lines.append("Installed version %s" % version)
     lines.append("Eggs:")
@@ -267,7 +278,6 @@ def summarize(version, eggs):
     egg_info = generate_egg_info(eggs)
 
     for egg in egg_info.keys():
-
         lines.append("  %s: %s" % (egg, egg_info[egg]))
 
     success("\n".join(lines))
@@ -275,7 +285,6 @@ def summarize(version, eggs):
 
 @_contextmanager
 def virtualenv(user):
-
     activate_cmd = 'source ~%s/bin/activate' % user
 
     with nested(cd('~%s/' % user), prefix(activate_cmd)):
